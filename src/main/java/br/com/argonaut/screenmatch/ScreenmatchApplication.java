1f4faccb27648f69c4d42ecd66cddb1745420b7e
DanielLemmertz <danielmultimate5178@gmail.com>
2025-04-17 22:06:56 -0300

    Screenmatch Maven o grande começo!

--- src/main/java/br/com/argonaut/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/argonaut/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,24 @@
 package br.com.argonaut.screenmatch;
 
+import br.com.argonaut.screenmatch.service.ConsumeApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Xama!");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        var scanner = new Scanner(System.in);
+        var consumeApi = new ConsumeApi();
+        var json = consumeApi.GetData(scanner.nextLine());
+        System.out.println(json);
+    }
 }
--- src/main/java/br/com/argonaut/screenmatch/service/ConsumeApi.java
+++ src/main/java/br/com/argonaut/screenmatch/service/ConsumeApi.java
@@ -2,16 +2,24 @@ package br.com.argonaut.screenmatch.service;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class ConsumeApi {
     public String GetData(String address) throws IOException, InterruptedException {
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.omdbapi.com/?t=" + address + "&apikey=29b6d465")).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.omdbapi.com/?t=" + encodedAddress + "&apikey=29b6d465")).build();
+        HttpResponse<String> response = null;
 
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return response.body();
     }
 }
